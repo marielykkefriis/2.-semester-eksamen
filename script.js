aca4ee7e2f91d6bbfc2e1f711dea714066ac52bb
Isabella Boisen <isabellaboisen@laptop-71281jme.kea.local>
2019-12-10 09:15:53 +0100

    menu og footer

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
-    document.querySelector(".menuknap").addEventListener("click", toggleMenu);
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
 
 }
 
 
-
 function toggleMenu() {
     console.log("toggleMenu");
-    dosument.querySelector("#menu").classList.toggle("hidden");
+    document.querySelector("#menu").classList.toggle("hidden");
 
-    let erSkjult = document.querySelector("#menu").classList.contains("#hidden") >
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
-        if (erSkjult == true) {
-            document.querySelector(".menuknap").textContent = "☰";
-
-        } else {
-            document.querySelector(".menuknap").textContent = "X";
-        }
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
